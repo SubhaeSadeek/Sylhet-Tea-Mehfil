4328467294c7ca17001d2408505ebe09dc811141
SubhaeSadeek <nurun.nabi@outlook.com>
2024-03-07 09:44:53 +0600

    initialised mashaAllah

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            clifford: '#da373d',
+          }
+        }
+      }
+    }
+
+    // daisy UI theme
+    module.exports = {
+        //...
+        daisyui: {
+          themes: ["light", "dark", "cupcake"],
+        },
+      }
+  
